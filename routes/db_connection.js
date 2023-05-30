e679e467a3d7516c71812326a22abda1494bb834
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2023-05-30 13:18:02 +0530

    modified:   .gitignore 	modified:   routes/db_connection.js

--- routes/db_connection.js
+++ routes/db_connection.js
@@ -1,12 +1,5 @@
 const sql = require('mysql');
 const dotenv = require("dotenv").config();
-// const pool =createPool({
-//     host:'sql202.epizy.com',
-//     user:'epiz_34082873',
-//     password:'EUBHcmaeso',
-//     database:'epiz_34082873_enquirethecalender',
-//     connectionLimit:2
-// })
 const db = sql.createConnection({
     host:process.env.DATABASE_HOST,
     user:process.env.DATABASE_USER,
