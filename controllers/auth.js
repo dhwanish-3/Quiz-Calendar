e2c2f65c3dc3ffdbe28a35d1fb31fda2ff85403d
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2023-12-26 06:47:04 +0530

    Formatted these files with prettier

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,112 +5,127 @@ const { promisify } = require("util");
 
 const db = require("../routes/db_connection");
 
-exports.register = (req,res)=>{
-    console.log(req.body);
-    // const name=req.body.name;
-    // const email=req.body.email;
-    // const password=req.body.password;
-    const {name,email,password}=req.body;
+exports.register = (req, res) => {
+  console.log(req.body);
+  // const name=req.body.name;
+  // const email=req.body.email;
+  // const password=req.body.password;
+  const { name, email, password } = req.body;
 
-    db.query('SELECT email FROM user_details WHERE email = ?',[email],async(error,results)=>{
-        if(error){
-            console.log(error);
-        }else if (results.length>0){
-            // res.status(200).redirect("/");
-            // alert("Email already exists. Please choose a different email");
-            return res.render('index',{
-                message: 'That email is already in use'
-            });
+  db.query(
+    "SELECT email FROM user_details WHERE email = ?",
+    [email],
+    async (error, results) => {
+      if (error) {
+        console.log(error);
+      } else if (results.length > 0) {
+        // res.status(200).redirect("/");
+        // alert("Email already exists. Please choose a different email");
+        return res.render("index", {
+          message: "That email is already in use",
+        });
+      }
+      let hashedPassword = await bcrypt.hash(password, 3);
+      console.log(hashedPassword);
+      let sqlquery = `INSERT INTO user_details(name,email,password) VALUES ('${name}','${email}','${hashedPassword}')`;
+      db.query(sqlquery, (error, results) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(results);
+          res.status(200).redirect("/");
+          // alert("Successfully Registered");
+          // return res.status(200).redirect('/',{
+          //     message:'Successfully registered'
+          // });
         }
-        let hashedPassword = await bcrypt.hash(password,3);
-        console.log(hashedPassword);
-        let sqlquery=`INSERT INTO user_details(name,email,password) VALUES ('${name}','${email}','${hashedPassword}')`;
-        db.query(sqlquery,(error,results)=>{
-            if(error){
-                console.log(error);
-            }else {
-                console.log(results);
-                res.status(200).redirect("/");
-                // alert("Successfully Registered");
-                // return res.status(200).redirect('/',{
-                //     message:'Successfully registered'
-                // });
-            }
-        })
-        console.log("Register happy now..?");
-    })
-    
-}
+      });
+      console.log("Register happy now..?");
+    }
+  );
+};
 
 exports.login = async (req, res) => {
-    console.log(req.body);
-    try {
-        const { email, password } = req.body;
-        if (!email || !password) {
-            return res.status(400).render('index', {
-                message: "Please Provide an email and password"
-            });
-        }
-        db.query('SELECT * FROM user_details WHERE email = ?', [email], async (err, results) => {
-            console.log(results);
-            if (!results || !await bcrypt.compare(password, results[0].password)) {
-                res.status(401).render('index', {
-                    message: 'Email or Password is incorrect'
-                })
-            } else {
-                const id = results[0].id;
-
-                const token = jwt.sign({ id }, process.env.JWT_SECRET, {
-                    expiresIn: process.env.JWT_EXPIRES_IN
-                });
-
-                console.log("the token is " + token);
-
-                const cookieOptions = {
-                    expires: new Date(
-                        Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
-                    ),
-                    httpOnly: true
-                }
-                res.cookie('userSave', token, cookieOptions);
-                res.status(200).redirect("/");
-            }
-        })
-    } catch (err) {
-        console.log(err);
+  console.log(req.body);
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).render("index", {
+        message: "Please Provide an email and password",
+      });
     }
-}
+    db.query(
+      "SELECT * FROM user_details WHERE email = ?",
+      [email],
+      async (err, results) => {
+        console.log(results);
+        if (
+          !results ||
+          !(await bcrypt.compare(password, results[0].password))
+        ) {
+          res.status(401).render("index", {
+            message: "Email or Password is incorrect",
+          });
+        } else {
+          const id = results[0].id;
+
+          const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_EXPIRES_IN,
+          });
+
+          console.log("the token is " + token);
+
+          const cookieOptions = {
+            expires: new Date(
+              Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+            ),
+            httpOnly: true,
+          };
+          res.cookie("userSave", token, cookieOptions);
+          res.status(200).redirect("/");
+        }
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 exports.isLoggedIn = async (req, res, next) => {
-    if (req.cookies.userSave) {
-        try {
-            // 1. Verify the token
-            const decoded = await promisify(jwt.verify)(req.cookies.userSave,
-                process.env.JWT_SECRET
-            );
-            console.log(decoded);
+  if (req.cookies.userSave) {
+    try {
+      // 1. Verify the token
+      const decoded = await promisify(jwt.verify)(
+        req.cookies.userSave,
+        process.env.JWT_SECRET
+      );
+      console.log(decoded);
 
-            // 2. Check if the user still exist
-            db.query('SELECT * FROM users WHERE id = ?', [decoded.id], (err, results) => {
-                console.log(results);
-                if (!results) {
-                    return next();
-                }
-                req.user = results[0];
-                return next();
-            });
-        } catch (err) {
-            console.log(err)
+      // 2. Check if the user still exist
+      db.query(
+        "SELECT * FROM users WHERE id = ?",
+        [decoded.id],
+        (err, results) => {
+          console.log(results);
+          if (!results) {
             return next();
+          }
+          req.user = results[0];
+          return next();
         }
-    } else {
-        next();
+      );
+    } catch (err) {
+      console.log(err);
+      return next();
     }
-}
+  } else {
+    next();
+  }
+};
 exports.logout = (req, res) => {
-    res.cookie('userSave', 'logout', {
-        expires: new Date(Date.now() + 2 * 1000),
-        httpOnly: true
-    });
-    res.status(200).redirect("/");
-}
+  res.cookie("userSave", "logout", {
+    expires: new Date(Date.now() + 2 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).redirect("/");
+};
--- routes/pages.js
+++ routes/pages.js
@@ -2,65 +2,62 @@ const express = require("express");
 const router = express.Router();
 // for calender
 require("mysql");
-let listOfEvents=[];
+let listOfEvents = [];
 const db = require("../routes/db_connection");
-let date=new Date();
-let i=date.getDate();
-i=i-3;
-let currMonth=date.getMonth();
+let date = new Date();
+let i = date.getDate();
+i = i - 3;
+let currMonth = date.getMonth();
 currMonth++;
-let currYear=date.getFullYear();
+let currYear = date.getFullYear();
 
-async function getEvents(date){
-    // let date='2023-04-28';
-    console.log('the date was'+date);
-    let sqlquery=`SELECT * FROM quiz_calender WHERE date = ?`;
+async function getEvents(date) {
+  // let date='2023-04-28';
+  console.log("the date was" + date);
+  let sqlquery = `SELECT * FROM quiz_calender WHERE date = ?`;
 
-    db.query(sqlquery,[date],(err,results)=>{
-        if(err){
-            console.log(err);
-            // return null;
-        }else if(results.length==0){
-            console.log('no events today bro');
-            // return null;
-        }
-        else{     
-            if(results!=null){
-                let events=JSON.parse(results[0].events);
-                
-                console.log(events);
-                for(let i=0;i<events.length;i++){
-                    let eventQuery=`SELECT * FROM events WHERE id = ?`;
-                    db.query(eventQuery,[events[i]],(err,results)=>{
-                        if(err){
-                            console.log('events error');
-                            
-                        }else if (results.length!=0){
-                            console.log('these are events');
-                            // console.log(results[0]);
-                            listOfEvents.push(results[0]);
-                            console.log(listOfEvents);
-                        }
-                    });    // listOfEvents.push('this is event results');
-                }
+  db.query(sqlquery, [date], (err, results) => {
+    if (err) {
+      console.log(err);
+      // return null;
+    } else if (results.length == 0) {
+      console.log("no events today bro");
+      // return null;
+    } else {
+      if (results != null) {
+        let events = JSON.parse(results[0].events);
+
+        console.log(events);
+        for (let i = 0; i < events.length; i++) {
+          let eventQuery = `SELECT * FROM events WHERE id = ?`;
+          db.query(eventQuery, [events[i]], (err, results) => {
+            if (err) {
+              console.log("events error");
+            } else if (results.length != 0) {
+              console.log("these are events");
+              // console.log(results[0]);
+              listOfEvents.push(results[0]);
+              console.log(listOfEvents);
             }
+          }); // listOfEvents.push('this is event results');
         }
-        });
-        
+      }
+    }
+  });
 }
-router.get("/",async(req,res)=>{    
-    // for(let loops=0;loops<5;loops++){
-    //     let date=`${currYear}-${currMonth}-${i}`;
-    //     await getEvents(date);
-    //     if(i==31){
-    //         i=1;
-    //         currMonth++;
-    //     }else i++;
-    // }
-    res.render("index");
-})
+router.get("/", async (req, res) => {
+  // for(let loops=0;loops<5;loops++){
+  //     let date=`${currYear}-${currMonth}-${i}`;
+  //     await getEvents(date);
+  //     if(i==31){
+  //         i=1;
+  //         currMonth++;
+  //     }else i++;
+  // }
+  res.render("index");
+});
 
 // exports.method = router;
 // exports.otherMethod = listOfEvents;
 
-module.exports={router,listOfEvents};
+module.exports = { router, listOfEvents };
